1e180d69a2c60f979728cadd4d0cfd81c89d3e43
Pawel Boguta <paul.boguta@gmail.com>
2025-08-02 13:33:45 +0200

    lint

--- src/lib/analytics.tsx
+++ src/lib/analytics.tsx
@@ -1,13 +1,13 @@
-import { env } from '@/env';
 import Script from 'next/script';
+import { env } from '@/env';
 
 export function Analytics() {
-  if (env.NODE_ENV === 'development') return <></>;
+  if (env.NODE_ENV === 'development') { return <></>; }
 
   return (
-   <Script
-      defer
+    <Script
       data-website-id={env.NEXT_PUBLIC_UMAMI_TRACKING_ID}
+      defer
       src={`${env.NEXT_PUBLIC_UMAMI_URL}/script.js`}
     />
   );
