7a8ab5ca4ad0c60f6e07b463cb035bae194a651c
Pawel Boguta <paul.boguta@gmail.com>
2025-08-02 13:26:23 +0200

    add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { Analytics } from '@/lib/analytics';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -10,8 +11,11 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html className={inter.className} lang="en" suppressHydrationWarning>
-      <body className='flex min-h-screen flex-col'>
-        <RootProvider>{children}</RootProvider>
+      <body className="flex min-h-screen flex-col">
+        <RootProvider>
+          {children}
+          <Analytics />
+        </RootProvider>
       </body>
     </html>
   );
